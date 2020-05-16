6802c30d9a6079133951b33487433acd183f5263
Xuan Dai Nguyen <nguyenxndaidev@gmail.com>
2020-05-16 20:30:45 +0200

    * Fixed pub.dev policies in to_string_helper_generator * Updated to_string_helper_generator to version 0.1.4

---
+++ generator/example/example.md
@@ -0,0 +1,1 @@
+See examples from package [to_string_helper](https://pub.dev/packages/to_string_helper).
--- generator/lib/src/to_string_generator.dart
+++ generator/lib/src/to_string_generator.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
