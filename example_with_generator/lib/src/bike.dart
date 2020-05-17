14b879b1c615a735c36d4013444ca3638e014784
Xuan Dai Nguyen <nguyenxndaidev@gmail.com>
2020-05-17 12:39:45 +0200

    Implemented unit tests: customize globalInclude

--- example_with_generator/lib/src/bike.dart
+++ example_with_generator/lib/src/bike.dart
@@ -31,11 +31,51 @@ class EBike extends Bike {
 @ToString()
 class FischerEBike extends EBike {
   static String name = 'Fischer eBike';
+  static String _secondName = 'Fischer eBike Next Generation';
   String color = 'red';
   var _engine = 'Fischer';
+  int _gears = 7;
 
   @override
   String toString() {
     return _$fischerEBikeToString(this);
+  }
+}
+
+@ToString(globalInclude: Include(nullValue: false, static: true))
+class TrekEBike extends EBike {
+  static String name = 'Trek eBike';
+  static String _secondName = 'Trek eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Trek';
+  int _gears = 5;
+
+  @override
+  String toString() {
+    return _$trekEBikeToString(this);
+  }
+}
+
+@ToString(
+    globalInclude: Include(
+  nullValue: false,
+  nonStatic: false,
+  static: true,
+  public: false,
+  private: true,
+))
+class GiantEBike extends EBike {
+  static String name = 'Giant eBike';
+  static String _secondName = 'Giant eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Giant';
+  int _gears = 5;
+  int _wheelSize;
+
+  @override
+  String toString() {
+    return _$giantEBikeToString(this);
   }
 }
--- example_with_generator/test/src/bike_test.dart
+++ example_with_generator/test/src/bike_test.dart
@@ -2,17 +2,31 @@ import 'package:test/test.dart';
 import 'package:to_string_helper_example_with_generator/example.dart';
 
 void main() {
-  group('toString() - minimal default configuration', () {
-    test('Bike', () {
-      expect(Bike().toString(), 'Bike{hasEngine=false, wheels=2}');
+  group('toString()', () {
+    group('minimal default configuration', () {
+      test('Bike', () {
+        expect(Bike().toString(), 'Bike{hasEngine=false, wheels=2}');
+      });
+
+      test('EBike', () {
+        expect(EBike().toString(), 'EBike{hasEngine=true, color=null}');
+      });
+
+      test('FischerEBike', () {
+        expect(FischerEBike().toString(), 'FischerEBike{color=red}');
+      });
     });
 
-    test('EBike', () {
-      expect(EBike().toString(), 'EBike{hasEngine=true, color=null}');
-    });
+    group('customize globalInclude', () {
+      test('TrekEBike', () {
+        expect(TrekEBike().toString(),
+            'TrekEBike{TrekEBike.name=Trek eBike, color=black}');
+      });
 
-    test('FischerEBike', () {
-      expect(FischerEBike().toString(), 'FischerEBike{color=red}');
+      test('GiantEBike', () {
+        expect(GiantEBike().toString(),
+            'GiantEBike{GiantEBike._secondName=Giant eBike Next Generation}');
+      });
     });
   });
 }
