50408e65ef5b80cca9e94adaf8c03af14c862c97
Nguyen <nguyenxndaidev@gmail.com>
2020-05-17 14:12:10 +0200

    Added unit tests for `@ToString()` (#29)

--- example_with_generator/lib/src/bike.dart
+++ example_with_generator/lib/src/bike.dart
@@ -6,8 +6,8 @@ part 'bike.g.dart';
 
 @ToString()
 class Bike extends Vehicle {
-  final hasEngine = false;
   final wheels = 2;
+  final hasEngine = false;
 
   @override
   String toString() {
@@ -17,10 +17,113 @@ class Bike extends Vehicle {
 
 @ToString()
 class EBike extends Bike {
+  static String name;
   final hasEngine = true;
+  String color;
+  String _engine;
 
   @override
   String toString() {
     return _$eBikeToString(this);
+  }
+}
+
+@ToString()
+class FischerEBike extends EBike {
+  static String name = 'Fischer eBike';
+  static String _secondName = 'Fischer eBike Next Generation';
+  String color = 'red';
+  var _engine = 'Fischer';
+  int _gears = 7;
+
+  @override
+  String toString() {
+    return _$fischerEBikeToString(this);
+  }
+}
+
+@ToString(globalInclude: Include(nullValue: false, static: true))
+class TrekEBike extends EBike {
+  static String name = 'Trek eBike';
+  static String _secondName = 'Trek eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Trek';
+  int _gears = 5;
+
+  @override
+  String toString() {
+    return _$trekEBikeToString(this);
+  }
+}
+
+@ToString(
+    globalInclude: Include(
+  nullValue: false,
+  nonStatic: false,
+  static: true,
+  public: false,
+  private: true,
+))
+class GiantEBike extends EBike {
+  static String name = 'Giant eBike';
+  static String _secondName = 'Giant eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Giant';
+  int _gears = 5;
+  int _wheelSize;
+
+  @override
+  String toString() {
+    return _$giantEBikeToString(this);
+  }
+}
+
+@ToString(inclusion: {EBike: Include()})
+class HerculesEBike extends EBike {
+  static String name = 'Hercules eBike';
+  static String _secondName = 'Hercules eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Hercules';
+  int _gears = 5;
+  int _wheelSize;
+
+  @override
+  String toString() {
+    return _$herculesEBikeToString(this);
+  }
+}
+
+@ToString(inclusion: {EBike: Include(), Bike: Include()})
+class Hercules2EBike extends EBike {
+  static String name = 'Hercules2 eBike';
+  static String _secondName = 'Hercules2 eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Hercules2';
+  int _gears = 5;
+  int _wheelSize;
+
+  @override
+  String toString() {
+    return _$hercules2EBikeToString(this);
+  }
+}
+
+@ToString(inclusion: {Bike: Include(), EBike: Include()})
+class Hercules3EBike extends EBike {
+  static String name = 'Hercules3 eBike';
+  static String _secondName = 'Hercules3 eBike Next Generation';
+  String color = 'black';
+  String secondColor;
+  var _engine = 'Hercules3';
+  int _gears = 5;
+  int _wheelSize;
+
+  @override
+  String toString() {
+    return _$hercules3EBikeToString(this);
   }
 }
--- example_with_generator/test/src/bike_test.dart
+++ example_with_generator/test/src/bike_test.dart
@@ -2,12 +2,48 @@ import 'package:test/test.dart';
 import 'package:to_string_helper_example_with_generator/example.dart';
 
 void main() {
-  group('toString() - default minimal configuration', () {
-    test('Bike', () {
-      expect(Bike().toString(), 'Bike{hasEngine=false, wheels=2}');
+  group('@ToString()', () {
+    group('minimal default configuration', () {
+      test('Bike', () {
+        expect(Bike().toString(), 'Bike{wheels=2, hasEngine=false}');
+      });
+
+      test('EBike', () {
+        expect(EBike().toString(), 'EBike{hasEngine=true, color=null}');
+      });
+
+      test('FischerEBike', () {
+        expect(FischerEBike().toString(), 'FischerEBike{color=red}');
+      });
     });
-    test('EBike - inherited fields should not be printed', () {
-      expect(EBike().toString(), 'EBike{hasEngine=true}');
+
+    group('@ToString(globalInclude)', () {
+      test('TrekEBike', () {
+        expect(TrekEBike().toString(),
+            'TrekEBike{TrekEBike.name=Trek eBike, color=black}');
+      });
+
+      test('GiantEBike', () {
+        expect(GiantEBike().toString(),
+            'GiantEBike{GiantEBike._secondName=Giant eBike Next Generation}');
+      });
+    });
+
+    group('@ToString(inclusion)', () {
+      test('HerculesEBike', () {
+        expect(HerculesEBike().toString(),
+            'HerculesEBike{color=black, secondColor=null, hasEngine=true}');
+      });
+
+      test('Hercules2EBike', () {
+        expect(Hercules2EBike().toString(),
+            'Hercules2EBike{color=black, secondColor=null, hasEngine=true, wheels=2}');
+      });
+
+      test('Hercules3EBike', () {
+        expect(Hercules3EBike().toString(),
+            'Hercules3EBike{color=black, secondColor=null, wheels=2, hasEngine=true}');
+      });
     });
   });
 }
